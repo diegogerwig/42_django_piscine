5be8b00fb1d5c289f7be41dbc60e63bdd9cd815c
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-07-06 13:59:49 +0200

    WIP mod01

---
+++ Module_01/ex01/numbers.py
@@ -0,0 +1,15 @@
+def print_numbers(filename):
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+            numbers = content.replace(",", "\n").split()
+            for number in numbers:
+                print(number)
+    except FileNotFoundError:
+        print(f"❌ Error: The file '{filename}' was not found.")
+    except Exception as e:
+        print(f"❌ Error: {e}")
+
+
+if __name__ == "__main__":
+    print_numbers("numbers.txt")
