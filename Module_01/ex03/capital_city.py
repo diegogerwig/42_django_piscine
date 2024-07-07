718d977d56aa739e2b164263121ccd3c86ac5f95
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-07-07 18:09:40 +0200

    WIP mod01

---
+++ Module_01/ex03/capital_city.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def print_capital(state):
+    states = {
+        "Oregon": "OR",
+        "Alabama": "AL",
+        "New Jersey": "NJ",
+        "Colorado": "CO"
+    }
+    capital_cities = {
+        "OR": "Salem",
+        "AL": "Montgomery",
+        "NJ": "Trenton",
+        "CO": "Denver",
+    }
+    if state in states:
+        value = states[state]
+        print(capital_cities[value])
+    else:
+        print("Unknown state")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        print_capital(sys.argv[1])
