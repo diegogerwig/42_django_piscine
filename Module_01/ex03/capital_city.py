6b788dadeb2735963a1cbea75b2001b714a16fa8
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-08-27 18:26:10 +0200

    update mod_01 ex03

--- Module_01/ex03/capital_city.py
+++ Module_01/ex03/capital_city.py
@@ -3,29 +3,25 @@ import sys
 
 def print_capital(state):
     states = {
-        "Oregon": "OR",
-        "Alabama": "AL",
-        "New Jersey": "NJ",
-        "Colorado": "CO"
+        'Oregon': 'OR',
+        'Alabama': 'AL',
+        'New Jersey': 'NJ',
+        'Colorado': 'CO'
     }
     capital_cities = {
-        "OR": "Salem",
-        "AL": "Montgomery",
-        "NJ": "Trenton",
-        "CO": "Denver",
+        'OR': 'Salem',
+        'AL': 'Montgomery',
+        'NJ': 'Trenton',
+        'CO': 'Denver',
     }
 
     if state in states:
         value = states[state]
         print(capital_cities[value])
     else:
-        print("Unknown state")
+        print('Unknown state')
 
 
-def main():
+if __name__ == '__main__':
     if len(sys.argv) == 2:
         print_capital(sys.argv[1])
-
-
-if __name__ == "__main__":
-    main()
--- Module_01/ex03/test_03.py
+++ Module_01/ex03/test_03.py
@@ -2,32 +2,36 @@ import subprocess
 
 
 def print_header(title):
-    border = "*" * (len(title) + 10)
+    border = '*' * (len(title) + 10)
     print('\n')
     print(border)
-    print(f"   🔨  {title}")
+    print(f'   🔨  {title}')
     print(border)
 
 
 def test_script():
-    print_header("Testing with valid state 'Oregon'")
-    subprocess.run(["python3", "capital_city.py", "Oregon"], check=True)
+    print_header('Testing with valid state: Oregon')
+    subprocess.run(['python3', 'capital_city.py', 'Oregon'], check=True)
 
-    print_header("Testing with invalid state 'alabama'")
-    subprocess.run(["python3", "capital_city.py", "alabama"], check=True)
+    print_header('Testing with unknown state: Ile-De-France')
+    subprocess.run(['python3', 'capital_city.py', 'Oregon'], check=True)
 
-    print_header("Testing with unknown state 'Bizkaia'")
-    subprocess.run(["python3", "capital_city.py", "Bizkaia"], check=True)
+    print_header('Testing without any arguments')
+    subprocess.run(['python3', 'capital_city.py'], check=True)
 
-    print_header("Testing with empty string")
-    subprocess.run(["python3", "capital_city.py", ""], check=True)
+    print_header('Testing multiple arguments: Oregon, Alabama')
+    subprocess.run(['python3', 'capital_city.py',
+                    'Oregon', 'Alabama'], check=True)
 
-    print_header("Testing without any arguments")
-    subprocess.run(["python3", "capital_city.py"], check=True)
+    print_header('Testing with empty string')
+    subprocess.run(['python3', 'capital_city.py', ''], check=True)
 
-    print_header("Testing multiple arguments: 'Oregon', 'Alabama'")
-    subprocess.run(["python3", "capital_city.py",
-                    "Oregon", "Alabama"], check=True)
+    print_header('Testing with invalid state: alabama')
+    subprocess.run(['python3', 'capital_city.py', 'alabama'], check=True)
+
+    print_header('Testing with unknown state: Bizkaia')
+    subprocess.run(['python3', 'capital_city.py', 'Bizkaia'], check=True)
+
 
 
 def main():
