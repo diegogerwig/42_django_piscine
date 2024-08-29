c266542475949fd62a9a194d710fa92ca9a70fc4
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-08-29 18:16:09 +0200

    update mod_02 ex00

--- Module_02/ex00/settings.py
+++ Module_02/ex00/settings.py
@@ -1,5 +1,5 @@
-page_title = '💫 Diego Gerwig\'s CV'
-name = 'Diego'
-surname = 'Gerwig'
+page_title = "💫 Diego Gerwig's CV"
+name = "Diego"
+surname = "Gerwig"
 age = 49
-profession = 'civil engineer'
+profession = "civil engineer"
