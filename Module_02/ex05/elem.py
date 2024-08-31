ea99a3c48505d53c1f1af45a485f259818d83ae5
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-08-31 13:14:29 +0200

    update mod_02 ex05

--- Module_02/ex05/elem.py
+++ Module_02/ex05/elem.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class Text(str): # str is a built-in Python class that represents a string of characters. The Text class is a subclass of str.
+class Text(str):  # str is a built-in Python class that represents a string of characters. The Text class is a subclass of str.
     """
     A Text class to represent a text you could use with your HTML elements.
 
@@ -13,7 +13,7 @@ class Text(str): # str is a built-in Python class that represents a string of ch
         """
 
         # Convert special characters to HTML entities
-        text = super().__str__().replace('<', '&lt;').replace('>', '&gt;') # super() initializes the parent class.
+        text = super().__str__().replace('<', '&lt;').replace('>', '&gt;')  # super() initializes the parent class.
 
         # Handle the special case for a single double quote
         if text == '"':
--- Module_02/ex05/elements.py
+++ Module_02/ex05/elements.py
@@ -5,77 +5,96 @@ class Html(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="html", attr=attr, content=content, tag_type="double")
 
+
 class Head(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="head", attr=attr, content=content, tag_type="double")
+
 
 class Body(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="body", attr=attr, content=content, tag_type="double")
 
+
 class Title(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="title", attr=attr, content=content, tag_type="double")
+
 
 class Meta(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="meta", attr=attr, content=content, tag_type="simple")
 
+
 class Img(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="img", attr=attr, content=content, tag_type="simple")
+
 
 class Table(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="table", attr=attr, content=content, tag_type="double")
 
+
 class Th(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="th", attr=attr, content=content, tag_type="double")
+
 
 class Tr(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="tr", attr=attr, content=content, tag_type="double")
 
+
 class Td(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="td", attr=attr, content=content, tag_type="double")
+
 
 class Ul(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="ul", attr=attr, content=content, tag_type="double")
 
+
 class Ol(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="ol", attr=attr, content=content, tag_type="double")
+
 
 class Li(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="li", attr=attr, content=content, tag_type="double")
 
+
 class H1(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="h1", attr=attr, content=content, tag_type="double")
+
 
 class H2(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="h2", attr=attr, content=content, tag_type="double")
 
+
 class P(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="p", attr=attr, content=content, tag_type="double")
+
 
 class Div(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="div", attr=attr, content=content, tag_type="double")
 
+
 class Span(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="span", attr=attr, content=content, tag_type="double")
 
+
 class Hr(Elem):
     def __init__(self, content=None, attr={}):
         super().__init__(tag="hr", attr=attr, content=content, tag_type="simple")
+
 
 class Br(Elem):
     def __init__(self, content=None, attr={}):
@@ -83,7 +102,7 @@ class Br(Elem):
 
 
 def create_html():
-    print( Html( [Head(), Body()] ) )
+    print(Html([Head(), Body()]))
 
     print('\n' + '='*50 + '\n')
 
