f725ced1f77585d1dbba65bfc25442d18f225929
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-09-12 17:30:13 +0200

    mod03 ex04

--- Module_03/ex04/my_script.sh
+++ Module_03/ex04/my_script.sh
@@ -7,8 +7,8 @@ source ~/sgoinfre/django_venv/bin/activate
 
 if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo '✅ Virtual environment activated.'
-    echo '   Installing requirements...'
-    echo "   Python version: $(which python)"
+    echo '⚙️ Installing requirements...'
+    echo "💻 Python version: $(which python)"
 else
     echo '❌ Failed to activate virtual environment.'
 fi
