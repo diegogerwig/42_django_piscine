8bd2a7a8d47053732ae1b73924ceae2636cfc984
Diego Gerwig <diegogerwig@gmail.com>
2024-09-14 22:28:05 +0200

    mod04 ex00

--- Module_04/src/clean_repo.sh
+++ Module_04/src/clean_repo.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf ~/sgoinfre/django_venv
-rm -rf ./d04
+echo '🧹 Cleaning repository...'
 
-echo '🗑️ Cleaned repository.'
+paths_to_remove=(
+    "~/sgoinfre/django_venv"
+    "./d04"
+)
+
+for path in "${paths_to_remove[@]}"; do
+    expanded_path=$(eval echo $path)
+    if [ -e "$expanded_path" ]; then
+        echo "   Removing: $expanded_path"
+        rm -rf "$expanded_path"
+    else
+        echo "   Path does not exist: $expanded_path"
+    fi
+done
+
+echo '🗑️  Cleaned repository.'
